34335a296fa63905bfd042ebb0eb90dbbe056951
MathewR25 <mremigio@fi.uba.ar>
2026-06-12 22:58:04 -0300

    Update ArbolABB.java

--- src/modelo/ciudad5/ArbolABB.java
+++ src/modelo/ciudad5/ArbolABB.java
@@ -8,17 +8,6 @@ public class ArbolABB {
         this.raiz = null;
     }
 
-    /**
-     * Inserta una palabra alfabéticamente en el árbol de forma jerárquica.
-     * * PRE: palabra != null (La cadena no puede estar vacía).
-     * linea y posicion deben ser mayores a 0.
-     * POST: la palabra se pasa a minúsculas, se limpia de espacios y se ubica 
-     * en el lugar que le corresponde, menores a la izquierda y mayores a la derecha.
-     * * @param palabra La palabra que se va a guardar.
-     * @param linea Número de línea del archivo de texto.
-     * @param posicion Ubicación de la palabra dentro de esa línea.
-     */
-    
     public void insertar(String palabra, int linea, int posicion) {
         String palabraLimpia = palabra.toLowerCase().trim();
         NodoArbol nuevoNodo = new NodoArbol(palabraLimpia, linea, posicion);
@@ -52,15 +41,6 @@ public class ArbolABB {
             }
         }
     }
-
-    /**
-     * Busca una palabra recorriendo las ramas del árbol y cuenta las operaciones.
-     * * PRE: palabraBuscada != null.
-     * POST: retorna el NodoArbol si encuentra la palabra o null en caso no exista. 
-     * Modifica el contador operacionesUltimaBusqueda con los ciclos dados.
-     * * @param palabraBuscada La palabra que queremos encontrar.
-     * @return NodoArbol El nodo que contiene la palabra y sus coordenadas, o null.
-     */
 
     public NodoArbol buscar(String palabraBuscada) {
         operacionesUltimaBusqueda = 0;
