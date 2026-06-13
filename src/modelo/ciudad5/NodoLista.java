af6755470bd2a1e09b069c5ac38f106290e7e199
MathewR25 <mremigio@fi.uba.ar>
2026-06-12 22:56:19 -0300

    Clean up comments in NodoLista.java
    Removed comments and documentation from NodoLista class.

--- src/modelo/ciudad5/NodoLista.java
+++ src/modelo/ciudad5/NodoLista.java
@@ -1,10 +1,5 @@
 package modelo.ciudad5;
 
-/**
- * Clase que representa un nodo (o vagón) dentro de la Lista Dinámica.
- * Guarda la palabra, sus coordenadas y se conecta con el siguiente elemento.
- */
-
 public class NodoLista {
     public String palabra;
     public int linea;
@@ -12,13 +7,6 @@ public class NodoLista {
 
     public NodoLista siguiente;
 
-    /**
-     * Constructor
-     * * @param palabra La palabra limpia que se va a guardar.
-     * @param linea El número de línea del archivo donde apareció.
-     * @param posicion El número de palabra dentro de esa línea.
-     */
-    
     public NodoLista(String palabra, int linea, int posicion) {
         this.palabra = palabra;
         this.linea = linea;
