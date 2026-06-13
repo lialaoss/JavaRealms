645c3b9c1e4aba32e25582fadd7857d6c52134f0
MathewR25 <mremigio@fi.uba.ar>
2026-06-12 22:57:23 -0300

    Update ListaDinamica.java

--- src/modelo/ciudad5/ListaDinamica.java
+++ src/modelo/ciudad5/ListaDinamica.java
@@ -8,29 +8,11 @@ public class ListaDinamica {
         this.cabeza = null;
     }
 
-    /**
-     * Inserta un nuevo elemento al principio de la lista enlazada.
-     * * PRE: palabra != null.
-     * POST: El nuevo vágon de datos se engancha en la cabeza de la lista.
-     * * @param palabra Texto a almacenar.
-     * @param linea Línea del archivo de origen.
-     * @param posicion Orden de la palabra en la línea.
-     */
-    
     public void insertar(String palabra, int linea, int posicion) {
         NodoLista nuevoVagon = new NodoLista(palabra, linea, posicion);
         nuevoVagon.siguiente = cabeza;
         cabeza = nuevoVagon;
     }
-
-    /**
-     * Busca una palabra de forma lineal, revisando nodo por nodo desde el inicio,
-     * * PRE: palabraBuscada != null.
-     * POST: Retorna el NodoLista si lo halla o null si llega al final de la lista
-     * Deja guardada la cantidad total de comparaciones realizadas
-     * * @param palabraBuscada Texto que se desea buscar.
-     * @return NodoLista Referencia del nodo encontrado o null si no existe.
-     */
     
     public NodoLista buscarLineal(String palabraBuscada) {
         operacionesUltimaBusqueda = 0;
